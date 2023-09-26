b2078564b27f46312d2c01fb5d9911973e6b1190
Maximilian Gruber <maximilian.gruber@ptb.de>
2023-09-26 10:30:07 +0200

    add powershell script to generate all ttl files

---
+++ generate_turtle_files.ps1
@@ -0,0 +1,29 @@
+# Please activate your dedicated Python-environment before executing this script
+
+Set-Variable -Name "PYTHONPATH" -Value "."
+
+# currently all files are generated into "./Testing/API/"
+
+echo "generating si.ttl + quantities.ttl"
+python ./CUQ/Python/Quantities_ABox.py
+echo "Done.`n"
+
+echo "generating units.ttl"
+python ./CUQ/Python/Units_ABox.py
+echo "Done.`n"
+
+echo "generating constants.ttl"
+python ./CUQ/Python/Constants_ABox.py
+echo "Done.`n"
+
+#echo "generating bodies.ttl"
+# TODO: python ./ResBod/Python/ResBod_TBox.py
+#echo "Done.`n"
+
+echo "generating cgpm.ttl"
+python ./ResBod/Python/ResBod_ABox_CGPM.py
+echo "Done.`n"
+
+echo "generating cipm.ttl"
+python ./ResBod/Python/ResBod_ABox_CIPM.py
+echo "Done.`n"
