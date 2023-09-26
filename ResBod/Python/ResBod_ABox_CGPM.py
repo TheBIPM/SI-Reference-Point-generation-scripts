f037541e04c626bf1f973ceeb6cc98c4f44d52b8
Maximilian Gruber <maximilian.gruber@ptb.de>
2023-09-26 14:29:46 +0200

    major overhaul of cgpm.ttl generation
    change namespace of rb
    introduce namespace cgpm
    declare outcomes to be of more specific types declaratio or resolution

--- ResBod/Python/ResBod_ABox_CGPM.py
+++ ResBod/Python/ResBod_ABox_CGPM.py
@@ -15,36 +15,40 @@ import yaml
 from rdflib import Graph, Literal, Namespace, URIRef
 from rdflib.namespace import DCTERMS, OWL, RDF, RDFS, SKOS, XSD
 from ResBod_ABox_CIPM import RB, get_outcome_values, sort_key
-from ResBod_TBox import ResBod_ns
 
 from settings import *
 
+# further needed namespaces
+CGPM = Namespace(SIURL + "bodies/CGPM#")
 
-# relative paths from this file
-BASE_PATH_FR = CGPM_FILES_FOLDER + "meetings-fr/"
-BASE_PATH_EN = CGPM_FILES_FOLDER + "meetings-en/"
 
-# Annotations to the ontology (name, Version number)
-PDF.g.add((URIRef(ResBod_ns), RDF.type, OWL.Ontology))
-PDF.g.add((URIRef(ResBod_ns), SKOS.prefLabel, Literal("SI Reference Point - Responsible Bodies", datatype=XSD.string)))
-PDF.g.add((URIRef(ResBod_ns), RDFS.comment,
-           Literal("Ontology, part of the SI Reference Point, covering the Responsible Bodies and their resolutions, "
-                   "decisions, etc (for the moment CGPM only)", datatype=XSD.string)))
-PDF.g.add((URIRef(ResBod_ns), DCTERMS.created, Literal(str(date.today()), datatype=XSD.date)))
+def main():
+    # init graph
+    g = Graph()
+    g.bind("rb", RB)
+    g.bind("cgpm", CGPM)
 
-# 1) CGPM
+    # relative paths from this file
+    BASE_PATH_FR = CGPM_FILES_FOLDER + "meetings-fr/"
+    BASE_PATH_EN = CGPM_FILES_FOLDER + "meetings-en/"
 
-# create the responsible body "CGPM"
-cgpm_URI = URIRef(ResBod_ns + "CGPM")
-PDF.g.add((URIRef(cgpm_URI), RDF.type, PDF.ResBod))
+    # Annotations to the ontology (name, Version number)
+    g.add((URIRef(CGPM), RDF.type, OWL.Ontology))
+    g.add((URIRef(CGPM), SKOS.prefLabel, Literal("SI Reference Point - CGPM meetings and outcomes", datatype=XSD.string)))
+    g.add((URIRef(CGPM), RDFS.comment,
+            Literal("Ontology, part of the SI Reference Point, covering the resolutions, "
+                    "decisions, etc of the CGPM", datatype=XSD.string)))
+    g.add((URIRef(CGPM), DCTERMS.created, Literal(str(date.today()), datatype=XSD.date)))
 
-# iterate over the resolutions files
-i = 1
-while i <= 27:
-    yaml_filename_fr = BASE_PATH_FR + "meeting-{:02d}.yml".format(i)
-    yaml_filename_en = BASE_PATH_EN + "meeting-{:02d}.yml".format(i)
+    # create the responsible body "CGPM"
+    g.add((URIRef(CGPM.CGPM), RDF.type, RB.ResBod))
 
-    try:
+    # iterate over the resolutions files (in 'natural' sorted order)
+    yaml_filenames_en = sorted(glob.glob(BASE_PATH_EN + "meeting-*.yml"), key = sort_key)
+    yaml_filenames_fr = sorted(glob.glob(BASE_PATH_FR + "meeting-*.yml"), key = sort_key)
+
+    for yaml_filename_en, yaml_filename_fr in zip(yaml_filenames_en, yaml_filenames_fr):
+
         with open(yaml_filename_fr, '+r', encoding='utf-8') as fr_file:
             meeting_fr = yaml.safe_load(fr_file)
         with open(yaml_filename_en, '+r', encoding='utf-8') as en_file:
@@ -52,101 +56,83 @@ while i <= 27:
 
         # if both (en and fr) readings were successful, proceed to extract the information on the conference
         # and create the assertions
-        conf_URI = URIRef(ResBod_ns + "CGPM" + str(meeting_fr['metadata']['identifier']))
+        conf_URI = CGPM.term(f"CGPM{meeting_fr['metadata']['identifier']}")
         conf_date = meeting_fr['metadata']['date']
         conf_Nr = meeting_fr['metadata']['identifier']
         conf_title_fr = meeting_fr['metadata']['title']
         conf_title_en = meeting_en['metadata']['title']
 
         # attach the Conference to the responsible body
-        PDF.g.add((cgpm_URI, PDF.hasEvent, conf_URI))
+        g.add((URIRef(CGPM.CGPM), RB.hasEvent, conf_URI))
 
         # add the information about the event
-        PDF.g.add((conf_URI, RDF.type, PDF.Event))
-        PDF.g.add((conf_URI, PDF.hasEventDate, Literal(conf_date, datatype=XSD.date)))
-        PDF.g.add((conf_URI, PDF.hasEventNr, Literal(conf_Nr, datatype=XSD.int)))
-        PDF.g.add((conf_URI, SKOS.prefLabel, Literal(conf_title_fr, lang="fr")))
-        PDF.g.add((conf_URI, SKOS.prefLabel, Literal(conf_title_en, lang="en")))
-        PDF.g.add((conf_URI, SKOS.hiddenLabel, Literal("CGPM" + str(conf_Nr), datatype=XSD.string)))
+        g.add((conf_URI, RDF.type, RB.Event))
+        g.add((conf_URI, RB.hasEventDate, Literal(conf_date, datatype=XSD.date)))
+        g.add((conf_URI, RB.hasEventNr, Literal(conf_Nr))) #, datatype=XSD.int)))
+        g.add((conf_URI, SKOS.prefLabel, Literal(conf_title_fr, lang="fr")))
+        g.add((conf_URI, SKOS.prefLabel, Literal(conf_title_en, lang="en")))
+        g.add((conf_URI, SKOS.hiddenLabel, Literal("CGPM" + str(conf_Nr), datatype=XSD.string)))
+
 
         # insert the assertion about the resolutions (attached to the responsible body)
         # in French
-        for resolution in meeting_fr['resolutions']:
-            resol_URI = None
-            resol_hidden_label = None
-            if resolution['type'] == 'resolution':
-                resol_URI = URIRef(ResBod_ns + "CGPM" + str(conf_Nr) + "-Res" + str(resolution['identifier']))
-                resol_hidden_label = "CGPM" + str(conf_Nr) + "-Res" + str(resolution['identifier'])
-            elif resolution['type'] == 'declaration':
-                if resolution['identifier'] == 0:
-                    resol_URI = URIRef(ResBod_ns + "CGPM" + str(conf_Nr) + "-Decl")
-                    resol_hidden_label = "CGPM" + str(conf_Nr) + "-Decl"
-                else:
-                    resol_URI = URIRef(ResBod_ns + "CGPM" + str(conf_Nr) + "-Decl" + str(resolution['identifier']))
-                    resol_hidden_label = "CGPM" + str(conf_Nr) + "-Decl" + str(resolution['identifier'])
-            resol_fr_DOI = resolution['url']
-            resol_title_fr = resolution['title']
-            resol_nr = resolution['identifier']
+        for outcome in meeting_fr['resolutions']:
 
-            PDF.g.add((cgpm_URI, PDF.hasAdopted, resol_URI))
-            PDF.g.add((resol_URI, PDF.wasAdoptedBy, cgpm_URI))
-            PDF.g.add((resol_URI, RDF.type, PDF.Outcome))
-            PDF.g.add((conf_URI, PDF.hasOutcome, resol_URI))
-            PDF.g.add((resol_URI, PDF.isOutcomeOf, conf_URI))
-            PDF.g.add((resol_URI, PDF.hasOutcomeTitle, Literal(resol_title_fr, lang='fr')))
-            PDF.g.add((resol_URI, PDF.hasOutcomeNr, Literal(resol_nr)))
-            PDF.g.add((resol_URI, SKOS.hiddenLabel, Literal(resol_hidden_label, datatype=XSD.string)))
-            PDF.g.add((resol_URI, PDF.hasDOI, Literal(resol_fr_DOI, lang='fr')))
-            if resolution['considerations']:  # only include this if there are considerations
-                considering_blankNodeID = URIRef(resol_URI + "Considering")  # Blank node holding the considerings
-                PDF.g.add((resol_URI, PDF.hasConsidering, considering_blankNodeID))
-                for consideration in resolution['considerations']:
-                    PDF.g.add(
-                        (considering_blankNodeID, PDF.hasConsideringText, Literal(consideration['message'], lang='fr')))
+            outcome_URI, out_id, hidden_label, outcome_type = get_outcome_values(outcome, conf_Nr, CGPM, "CGPM")
 
-            if resolution['actions']:  # only include this if there are actions
-                actions_blankNodeID = URIRef(resol_URI + "Action")  # Blank node holding the actions
-                PDF.g.add((resol_URI, PDF.hasAction, actions_blankNodeID))
-                for action in resolution['actions']:
-                    PDF.g.add((actions_blankNodeID, PDF.hasActionText, Literal(action['message'], lang='fr')))
+            outcome_fr_DOI = outcome['url']
+            outcome_title_fr = outcome['title']
+
+            g.add((URIRef(CGPM.CGPM), RB.hasAdopted, outcome_URI))
+            g.add((outcome_URI, RB.wasAdoptedBy, URIRef(CGPM.CGPM)))
+            g.add((outcome_URI, RDF.type, outcome_type))
+            g.add((conf_URI, RB.hasOutcome, outcome_URI))
+            g.add((outcome_URI, RB.isOutcomeOf, conf_URI))
+            g.add((outcome_URI, RB.hasOutcomeTitle, Literal(outcome_title_fr, lang='fr')))
+            g.add((outcome_URI, RB.hasOutcomeNr, Literal(out_id)))
+            g.add((outcome_URI, SKOS.hiddenLabel, Literal(hidden_label, datatype=XSD.string)))
+            g.add((outcome_URI, RB.hasDOI, Literal(outcome_fr_DOI, lang='fr')))
+            if outcome['considerations']:  # only include this if there are considerations
+                considering_blankNodeID = URIRef(outcome_URI + "Considering")  # Blank node holding the considerings
+                g.add((outcome_URI, RB.hasConsidering, considering_blankNodeID))
+                for consideration in outcome['considerations']:
+                    g.add(
+                        (considering_blankNodeID, RB.hasConsideringText, Literal(consideration['message'], lang='fr')))
+
+            if outcome['actions']:  # only include this if there are actions
+                actions_blankNodeID = URIRef(outcome_URI + "Action")  # Blank node holding the actions
+                g.add((outcome_URI, RB.hasAction, actions_blankNodeID))
+                for action in outcome['actions']:
+                    g.add((actions_blankNodeID, RB.hasActionText, Literal(action['message'], lang='fr')))
 
         # in English
-        for resolution in meeting_en['resolutions']:
-            resol_URI = None
-            resol_hidden_label = None
-            if resolution['type'] == 'resolution':
-                resol_URI = URIRef(ResBod_ns + "CGPM" + str(conf_Nr) + "-Res" + str(resolution['identifier']))
-                resol_hidden_label = "CGPM" + str(conf_Nr) + "-Res" + str(resolution['identifier'])
-            elif resolution['type'] == 'declaration':
-                if resolution['identifier'] == 0:
-                    resol_URI = URIRef(ResBod_ns + "CGPM" + str(conf_Nr) + "-Decl")
-                    resol_hidden_label = "CGPM" + str(conf_Nr) + "-Decl"
-                else:
-                    resol_URI = URIRef(ResBod_ns + "CGPM" + str(conf_Nr) + "-Decl" + str(resolution['identifier']))
-                    resol_hidden_label = "CGPM" + str(conf_Nr) + "-Decl" + str(resolution['identifier'])
-            resol_en_DOI = resolution['url']
-            resol_title_en = resolution['title']
-            considering_blankNodeID = URIRef(resol_URI + "Considering")
-            actions_blankNodeID = URIRef(resol_URI + "Action")
-            PDF.g.add((resol_URI, PDF.hasOutcomeTitle, Literal(resol_title_en, lang='en')))
-            PDF.g.add((resol_URI, PDF.hasDOI, Literal(resol_en_DOI, lang='en')))
-            if resolution['considerations']:  # only include this if there are considerations
-                considering_blankNodeID = URIRef(resol_URI + "Considering")  # Blank node holding the considerings
-                PDF.g.add((resol_URI, PDF.hasConsidering, considering_blankNodeID))
-                for consideration in resolution['considerations']:
-                    PDF.g.add(
-                        (considering_blankNodeID, PDF.hasConsideringText, Literal(consideration['message'], lang='en')))
+        for outcome in meeting_en['resolutions']:
+            
+            outcome_URI, out_id, hidden_label, outcome_type = get_outcome_values(outcome, conf_Nr, CGPM, "CGPM")
 
-            if resolution['actions']:  # only include this if there are actions
-                actions_blankNodeID = URIRef(resol_URI + "Action")  # Blank node holding the actions
-                PDF.g.add((resol_URI, PDF.hasAction, actions_blankNodeID))
-                for action in resolution['actions']:
-                    PDF.g.add((actions_blankNodeID, PDF.hasActionText, Literal(action['message'], lang='en')))
+            resol_en_DOI = outcome['url']
+            resol_title_en = outcome['title']
+            considering_blankNodeID = URIRef(outcome_URI + "Considering")
+            actions_blankNodeID = URIRef(outcome_URI + "Action")
+            g.add((outcome_URI, RB.hasOutcomeTitle, Literal(resol_title_en, lang='en')))
+            g.add((outcome_URI, RB.hasDOI, Literal(resol_en_DOI, lang='en')))
+            if outcome['considerations']:  # only include this if there are considerations
+                considering_blankNodeID = URIRef(outcome_URI + "Considering")  # Blank node holding the considerings
+                g.add((outcome_URI, RB.hasConsidering, considering_blankNodeID))
+                for consideration in outcome['considerations']:
+                    g.add(
+                        (considering_blankNodeID, RB.hasConsideringText, Literal(consideration['message'], lang='en')))
 
-        # go to next CGPM
-    except FileNotFoundError:
-        pass  # any files not present are not available
-    i += 1
+            if outcome['actions']:  # only include this if there are actions
+                actions_blankNodeID = URIRef(outcome_URI + "Action")  # Blank node holding the actions
+                g.add((outcome_URI, RB.hasAction, actions_blankNodeID))
+                for action in outcome['actions']:
+                    g.add((actions_blankNodeID, RB.hasActionText, Literal(action['message'], lang='en')))
 
-# serialize the knowledge graph when all CGPMs are covered,         
-PDF.g.serialize(format='turtle', destination=APIPATH + 'cgpm.ttl')
+
+    # serialize the knowledge graph when all CGPMs are covered,         
+    g.serialize(format='turtle', destination=APIPATH + 'cgpm.ttl')
+
+
+if __name__ == "__main__":
+    main()
