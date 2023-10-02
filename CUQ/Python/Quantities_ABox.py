b968acf08f3a4733b7424e63d3bdac55202ee53a
Maximilian Gruber <maximilian.gruber@ptb.de>
2023-10-02 18:05:08 +0200

    quantities: add own namespace, remove si-base-onto

--- CUQ/Python/Quantities_ABox.py
+++ CUQ/Python/Quantities_ABox.py
@@ -8,23 +8,27 @@
 # export PYTHONPATH=(path where the package is located) (e.g. /Users/gregordudle/Development/Semantic-SI)
 #
 
-from rdflib import URIRef, Literal
+from rdflib import URIRef, Literal, Graph
 from rdflib.namespace import DCTERMS, RDF, RDFS, SKOS, OWL, XSD
 from CUQ_TBox import SiElements
 from datetime import date
 from settings import *
 import openpyxl
 
-PDF = SiElements()
-QUNURL = SIURL + "quantities"
 
+PDF = SiElements()
+g = Graph()
+
+# copy over all namespaces from PDF.g to g
+for key, val in PDF.g.namespaces():
+    g.bind(key, val) 
 
 # Annotations to the ontology (name, Version number)
-PDF.g.add((URIRef(QUNURL), RDF.type, OWL.Ontology))
-PDF.g.add((URIRef(QUNURL), SKOS.prefLabel, Literal("SI Reference Point - Quantities", datatype=XSD.string)))
-PDF.g.add((URIRef(QUNURL), RDFS.comment, Literal("Ontology, part of the SI reference point, covering quantities",
+g.add((URIRef(PDF.namespace_quantities), RDF.type, OWL.Ontology))
+g.add((URIRef(PDF.namespace_quantities), SKOS.prefLabel, Literal("SI Reference Point - Quantities", datatype=XSD.string)))
+g.add((URIRef(PDF.namespace_quantities), RDFS.comment, Literal("Ontology, part of the SI reference point, covering quantities",
                                                  datatype=XSD.string)))
-PDF.g.add((URIRef(QUNURL), DCTERMS.created, Literal(str(date.today()), datatype=XSD.date)))
+g.add((URIRef(PDF.namespace_quantities), DCTERMS.created, Literal(str(date.today()), datatype=XSD.date)))
 
 # crawl through the rows of the XLS file
 
@@ -38,20 +42,20 @@ for row in range(2, sheet.max_row):
     label_en = sheet.cell(row, column=2).value
     label_fr = sheet.cell(row, column=3).value
     
-    element = PDF.set_uri(identifier)
-    PDF.g.add((element, RDF.type, PDF.QuantityKind))
-    PDF.g.add((element, SKOS.prefLabel, Literal(label_en, lang="en")))
-    PDF.g.add((element, SKOS.prefLabel, Literal(label_fr, lang="fr")))
-    PDF.g.add((element, SKOS.altLabel, Literal(identifier, datatype=XSD.string)))
+    element = PDF.set_quantity_uri(identifier)
+    g.add((element, RDF.type, PDF.QuantityKind))
+    g.add((element, SKOS.prefLabel, Literal(label_en, lang="en")))
+    g.add((element, SKOS.prefLabel, Literal(label_fr, lang="fr")))
+    g.add((element, SKOS.altLabel, Literal(identifier, datatype=XSD.string)))
     if sheet.cell(row, column=4).value is not None:
         unit = sheet.cell(row, column=4).value
-        PDF.g.add((element, PDF.hasUnit, PDF.set_unit_uri(unit)))
+        g.add((element, PDF.hasUnit, PDF.set_unit_uri(unit)))
     if sheet.cell(row, column=5).value is not None:
         sameas = sheet.cell(row, column=5).value
-        PDF.g.add((element, OWL.sameAs, PDF.set_uri(sameas)))
+        g.add((element, OWL.sameAs, PDF.set_uri(sameas)))
     if sheet.cell(row, column=6).value is not None:
         sameas = sheet.cell(row, column=6).value
-        PDF.g.add((element, OWL.sameAs, PDF.set_uri(sameas)))
+        g.add((element, OWL.sameAs, PDF.set_uri(sameas)))
 
 
-PDF.g.serialize(format='turtle', destination=APIPATH + 'quantities.ttl')
+g.serialize(format='turtle', destination=APIPATH + 'quantities.ttl')
