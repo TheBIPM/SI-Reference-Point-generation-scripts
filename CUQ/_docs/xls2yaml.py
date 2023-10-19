7bf9e5681a0d511f81e31ae400cf6dd92aef1e62
Frédéric Meynadier <Frederic.Meynadier@bipm.org>
2023-10-19 15:09:18 +0000

    A proposal for mass-converting xls src to yaml

---
+++ CUQ/_docs/xls2yaml.py
@@ -0,0 +1,69 @@
+"""
+Transform excel input files into yaml
+"""
+
+import openpyxl
+from ruamel.yaml import YAML
+
+
+def sheet2yaml(sheet, output_name, special_case=None):
+    """ Assume first row is list of fields then each row is a record
+    """
+    output = []
+    # This returns something like 'A1:G25'
+    dim = sheet.calculate_dimension()
+    lr_cell = dim.split(':')[1]
+    last_col = ord(lr_cell[0]) - 64
+    last_row = int(lr_cell[1:])
+    first_row = 1
+    # Special cases - pre processing
+    if output_name == "def_collectors":
+        last_col = 5
+    if output_name == "si_units_special_names":
+        last_row = 23
+        last_col = 10
+    if output_name == "non_si_units":
+        first_row = 6
+    if output_name == "quantities":
+        # Dump "same as"
+        last_col = 4
+    # Get fields list by parsing first row
+    field_list = []
+    for col in range(1, last_col + 1):
+        field_list.append(sheet.cell(first_row, col).value)
+    for row in range(first_row + 1, last_row + 1):
+        buf = {}
+        for i, f in enumerate(field_list):
+            buf[f] = sheet.cell(row, i + 1).value
+        output.append(buf)
+    # Special cases - post processing
+    if output_name == "def_collectors":
+        # need to append list of definitions
+        for row in range(2, last_row + 1):
+            output[row - 2]['definitions'] = []
+            for col in range(6, 11):
+                defid = sheet.cell(row, col).value
+                if defid is not None:
+                    output[row - 2]['definitions'].append(defid)
+    yaml = YAML()
+    with open(output_name + '.yaml', 'w') as fp:
+        yaml.dump(output, fp)
+
+
+units_wb_obj = openpyxl.load_workbook('Units_Prefixes.xlsx')
+sheet2yaml(units_wb_obj['Prefixes'], 'prefixes')
+sheet2yaml(units_wb_obj['BaseUnitsDefs'], 'base_units_defs')
+sheet2yaml(units_wb_obj['DefCollectors'], 'def_collectors')
+sheet2yaml(units_wb_obj['SIUnitsSpecialNames'], 'si_units_special_names')
+sheet2yaml(units_wb_obj['NonSIUnits'], 'non_si_units')
+
+notes_wb_obj = openpyxl.load_workbook('Notes.xlsx')
+sheet2yaml(notes_wb_obj['en'], 'notes_en')
+sheet2yaml(notes_wb_obj['fr'], 'notes_fr')
+sheet2yaml(notes_wb_obj['symbols'], 'symbols')
+
+quantities_wb_obj = openpyxl.load_workbook('quantities.xlsx')
+sheet2yaml(quantities_wb_obj['Sheet1'], 'quantities')
+
+missing_def = openpyxl.load_workbook('missing_definitions_cipm.xlsx')
+sheet2yaml(missing_def['Feuil1'], 'missing_definitions_cipm')
