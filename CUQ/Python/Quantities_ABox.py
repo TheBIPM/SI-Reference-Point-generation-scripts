bc80f9264734a90521aaa6b05573faeba938220f
Frédéric Meynadier <Frederic.Meynadier@bipm.org>
2023-10-23 14:53:26 +0000

    Switch qty to yaml

--- CUQ/Python/Quantities_ABox.py
+++ CUQ/Python/Quantities_ABox.py
@@ -13,7 +13,8 @@ from rdflib.namespace import DCTERMS, RDF, RDFS, SKOS, OWL, XSD
 from CUQ_TBox import SiElements
 from datetime import date
 from settings import *
-import openpyxl
+from ruamel.yaml import YAML
+import os
 
 
 PDF = SiElements()
@@ -21,41 +22,29 @@ g = Graph()
 
 # copy over all namespaces from PDF.g to g
 for key, val in PDF.g.namespaces():
-    g.bind(key, val) 
+    g.bind(key, val)
 
 # Annotations to the ontology (name, Version number)
 g.add((URIRef(PDF.namespace_quantities), RDF.type, OWL.Ontology))
-g.add((URIRef(PDF.namespace_quantities), SKOS.prefLabel, Literal("SI Reference Point - Quantities", datatype=XSD.string)))
-g.add((URIRef(PDF.namespace_quantities), RDFS.comment, Literal("Ontology, part of the SI reference point, covering quantities",
-                                                 datatype=XSD.string)))
-g.add((URIRef(PDF.namespace_quantities), DCTERMS.created, Literal(str(date.today()), datatype=XSD.date)))
+g.add((URIRef(PDF.namespace_quantities), SKOS.prefLabel,
+       Literal("SI Reference Point - Quantities", datatype=XSD.string)))
+g.add((URIRef(PDF.namespace_quantities), RDFS.comment,
+       Literal("Ontology, part of the SI reference point, covering quantities",
+               datatype=XSD.string)))
+g.add((URIRef(PDF.namespace_quantities), DCTERMS.created,
+       Literal(str(date.today()), datatype=XSD.date)))
 
-# crawl through the rows of the XLS file
-
-# worksheet containing the basic information
-qty_wb_obj = openpyxl.load_workbook(XLS_FILES_FOLDER + 'quantities.xlsx')
-sheet = qty_wb_obj.active
-
-# crawl through the rows of the XLS file
-for row in range(2, sheet.max_row):
-    identifier = sheet.cell(row, column=1).value
-    label_en = sheet.cell(row, column=2).value
-    label_fr = sheet.cell(row, column=3).value
-    
-    element = PDF.set_quantity_uri(identifier)
-    g.add((element, RDF.type, PDF.QuantityKind))
-    g.add((element, SKOS.prefLabel, Literal(label_en, lang="en")))
-    g.add((element, SKOS.prefLabel, Literal(label_fr, lang="fr")))
-    g.add((element, SKOS.altLabel, Literal(identifier, datatype=XSD.string)))
-    if sheet.cell(row, column=4).value is not None:
-        unit = sheet.cell(row, column=4).value
-        g.add((element, PDF.hasUnit, PDF.set_unit_uri(unit)))
-    if sheet.cell(row, column=5).value is not None:
-        sameas = sheet.cell(row, column=5).value
-        g.add((element, OWL.sameAs, PDF.set_uri(sameas)))
-    if sheet.cell(row, column=6).value is not None:
-        sameas = sheet.cell(row, column=6).value
-        g.add((element, OWL.sameAs, PDF.set_uri(sameas)))
-
+# crawl through the items of the YAML file
+yaml = YAML()
+with open(os.path.join(XLS_FILES_FOLDER, 'quantities.yaml')) as fp:
+    qty_list = yaml.load(fp)
+for qty in qty_list:
+    element = PDF.set_quantity_uri(qty['identifier'])
+    g.add((element, SKOS.prefLabel, Literal(qty['quantity-en'], lang="en")))
+    g.add((element, SKOS.prefLabel, Literal(qty['quantity-fr'], lang="fr")))
+    g.add((element, SKOS.altLabel, Literal(qty['identifier'],
+                                           datatype=XSD.string)))
+    if 'Unit' in qty and qty['Unit'] is not None:
+        g.add((element, PDF.hasUnit, PDF.set_unit_uri(qty['Unit'])))
 
 g.serialize(format='turtle', destination=APIPATH + 'quantities.ttl')
