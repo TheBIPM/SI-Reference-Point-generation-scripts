6d5fddf45f3b25498e73b4d667805ca569a53ce3
Stuart Chalk <schalk@unf.edu>
2023-11-02 09:45:40 -0400

    Python formatting update for Constants_ABox.py

--- CUQ/Python/Constants_ABox.py
+++ CUQ/Python/Constants_ABox.py
@@ -8,8 +8,7 @@
 # export PYTHONPATH=(path where the package is located) (e.g. /Users/gregordudle/Development/Semantic-SI)
 #
 
-from rdflib import Graph, URIRef, Literal, BNode
-from rdflib.namespace import DCTERMS, RDF, RDFS, SKOS, OWL, XSD
+from rdflib import *
 from CUQ_TBox import SiElements
 from datetime import date
 from settings import *
