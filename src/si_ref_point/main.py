de53597e2daa2fd5fc565d7ca2b9fca69f5a5ae2
Maximilian Gruber <maximilian.gruber@ptb.de>
2023-11-07 09:25:08 +0100

    generate extend base vocab

--- src/si_ref_point/main.py
+++ src/si_ref_point/main.py
@@ -3,7 +3,7 @@
 
 import argparse
 import logging
-import si_ref_point.cuq.CUQ_TBox as CUQ_TBox
+import si_ref_point.cuq.CUQ_TBox_Extension as CUQ_TBox_Extension
 import si_ref_point.cuq.Quantities_ABox as Quantities_ABox
 import si_ref_point.cuq.Units_ABox as Units_ABox
 import si_ref_point.cuq.Constants_ABox as Constants_ABox
@@ -53,17 +53,19 @@ def main():
     else:
         logging.basicConfig(level=logging.INFO)
 
-    file_generator = {'si': CUQ_TBox.main,
-                      'quantities': Quantities_ABox.main,
-                      'units': Units_ABox.main,
-                      'constants': Constants_ABox.main,
-                      'prefixes': Prefixes_ABox.main,
-                      'decisions': gen_decisions.main,
-                      'bodies': ResBod_TBox.main,
-                      'cgpm': ResBod_ABox_CGPM.main,
-                      'cipm': ResBod_ABox_CIPM.main,
-                      'cctf': ResBod_ABox_CCTF.main,
-                      }
+    file_generator = {
+        #'si': CUQ_TBox.main,
+        'si': CUQ_TBox_Extension.main,
+        'quantities': Quantities_ABox.main,
+        'units': Units_ABox.main,
+        'constants': Constants_ABox.main,
+        'prefixes': Prefixes_ABox.main,
+        'decisions': gen_decisions.main,
+        'bodies': ResBod_TBox.main,
+        'cgpm': ResBod_ABox_CGPM.main,
+        'cipm': ResBod_ABox_CIPM.main,
+        'cctf': ResBod_ABox_CCTF.main,
+    }
     output = {}
     for label, generator in file_generator.items():
         if args.only and args.only not in label:
