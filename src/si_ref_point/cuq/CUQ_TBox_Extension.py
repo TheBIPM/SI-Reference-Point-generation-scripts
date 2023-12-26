3c5520bf31781c21f5084e7f44fc7f665385669a
Frédéric Meynadier <Frederic.Meynadier@bipm.org>
2023-12-26 17:23:52 +0000

    WIP not functional yet (ttl errors)
    Switch to TTL -based CUQ tbox

--- src/si_ref_point/cuq/CUQ_TBox_Extension.py
+++ src/si_ref_point/cuq/CUQ_TBox_Extension.py
@@ -1,18 +1,14 @@
 import os
+from rdflib import Graph
 
-from si_ref_point.cuq.CUQ_TBox import SiElements
-from si_ref_point.settings import APIPATH, CUQ_FILES_FOLDER
+from si_ref_point.settings import CUQ_FILES_FOLDER
 
 def main():
-    # load ontology as graph from the generating script
-    si_base_onto = SiElements()
-    g_ext = si_base_onto.g
-
-    # load extended concepts into same graph
-    extended_concepts_path = os.path.join(CUQ_FILES_FOLDER, "CUQ_extended_concepts.ttl")
-    g_ext.parse(extended_concepts_path, format="ttl")
-
-    return g_ext
+    g = Graph()
+    for ttl_file in ['CUQ_core_concepts.ttl',
+                     'CUQ_extended_concepts.ttl']:
+        g.parse(os.path.join(CUQ_FILES_FOLDER, ttl_file), format="ttl")
+    return g
 
 
 if __name__ == "__main__":
