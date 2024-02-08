7e24a8c657b8981229a62602916fa86221abc484
Frédéric Meynadier <Frederic.Meynadier@bipm.org>
2024-02-08 10:47:16 +0000

    Set numeric exponent to XSD.short in  UnitPower

--- src/si_ref_point/cuq/Units_ABox.py
+++ src/si_ref_point/cuq/Units_ABox.py
@@ -150,7 +150,7 @@ def transform_to_graph(expression, PDF, graph, symbols):
                 # insert base and exponent
                 graph, node = transform_to_graph(expression["exp"][0],
                                                  PDF, graph, symbols)
-                exponent = Literal(expression["exp"][1])
+                exponent = Literal(expression["exp"][1], datatype=XSD.short)
                 graph.add((expr_node, hasBase, node))
                 graph.add((expr_node, hasExponent, exponent))
 
