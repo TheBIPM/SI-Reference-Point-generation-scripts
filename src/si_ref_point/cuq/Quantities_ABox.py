3717f85f67d69a0c711e18b41a355fba6781ea5d
Frédéric Meynadier <Frederic.Meynadier@bipm.org>
2024-05-27 09:56:23 +0000

    Split quantities
    Input source for quantities is now split into core / other
    
    Additional checks in the script to verify that no identifier is a
    duplicate.

--- src/si_ref_point/cuq/Quantities_ABox.py
+++ src/si_ref_point/cuq/Quantities_ABox.py
@@ -9,6 +9,7 @@ from datetime import date
 from si_ref_point.settings import CUQ_FILES_FOLDER
 import yaml
 import os
+import logging
 
 
 
@@ -32,18 +33,25 @@ def main():
            Literal(str(date.today()), datatype=XSD.date)))
 
     # crawl through the items of the YAML file
-    with open(os.path.join(CUQ_FILES_FOLDER, 'quantities.yaml'), encoding="utf8") as fp:
-        qty_list = yaml.safe_load(fp)
-    for qty in qty_list:
-        element = PDF.set_quantity_uri(qty['identifier'])
-        g.add((element, RDF.type, PDF.QuantityKind))
-        g.add((element, SKOS.prefLabel,
-               Literal(qty['quantity-en'], lang="en")))
-        g.add((element, SKOS.prefLabel,
-               Literal(qty['quantity-fr'], lang="fr")))
-        g.add((element, SKOS.altLabel, Literal(qty['identifier'],
-                                               datatype=XSD.string)))
-        if 'Unit' in qty and qty['Unit'] is not None:
-            g, cmpnd_node = transform_to_graph(qty['Unit'], PDF, g)
-            g.add((element, PDF.hasUnit, cmpnd_node))
+    qty_files = ['quantities_core.yaml', 'quantities_other.yaml']
+    qty_code_list = []
+    for filename in qty_files:
+        with open(os.path.join(CUQ_FILES_FOLDER, filename), encoding="utf8") as fp:
+            qty_list = yaml.safe_load(fp)
+            for qty in qty_list:
+                if qty['identifier'] not in qty_code_list:
+                    qty_code_list.append(qty['identifier'])
+                else:
+                    logging.error("quantity %s already defined !" % qty['identifier'])
+                element = PDF.set_quantity_uri(qty['identifier'])
+                g.add((element, RDF.type, PDF.QuantityKind))
+                g.add((element, SKOS.prefLabel,
+                       Literal(qty['quantity-en'], lang="en")))
+                g.add((element, SKOS.prefLabel,
+                       Literal(qty['quantity-fr'], lang="fr")))
+                g.add((element, SKOS.altLabel, Literal(qty['identifier'],
+                                                       datatype=XSD.string)))
+                if 'Unit' in qty and qty['Unit'] is not None:
+                    g, cmpnd_node = transform_to_graph(qty['Unit'], PDF, g)
+                    g.add((element, PDF.hasUnit, cmpnd_node))
     return g
